6fcdb3380f7e390b7ca32d49e0e50c349d82c074
MarinJ <mjuric00@fesb.hr>
2020-02-12 23:26:42 +0100

    Adds gradient_arc_painter to dart the arc to indicate points / bonus points

--- lib/presentation/widgets/player_grid.dart
+++ lib/presentation/widgets/player_grid.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:game_counter/domain/entities/game.dart';
+import 'package:game_counter/presentation/widgets/gradient_arc_painter.dart';
 
 class PlayerGrid extends StatefulWidget {
   final Game currentGame;
@@ -46,21 +49,51 @@ class _PlayerGridState extends State<PlayerGrid> {
                         ),
                       ),
                     ),
-                    child: Center(
-                      child: Text(
-                        widget.currentGame.players[index].points.toString(),
-                        style: TextStyle(
-                          color: Colors.grey.shade700,
-                          fontSize: 32,
-                          shadows: [
-                            Shadow(
-                              blurRadius: 7.5,
-                              color: Colors.grey,
-                              offset: Offset(0.0, 2.5),
-                            ),
-                          ],
+                    child: Stack(
+                      children: <Widget>[
+                        Center(
+                          child: Container(
+                            height: 75,
+                            width: 75,
+                            child: CustomPaint(
+                                painter: GradientArcPainter(
+                              progress: 0.99,
+                              startColor: Colors.orange,
+                              endColor: Colors.white,
+                              width: 4.0,
+                            )),
+                          ),
                         ),
-                      ),
+                        Center(
+                          child: Container(
+                            height: 75,
+                            width: 75,
+                            child: CustomPaint(
+                                painter: GradientArcPainter(
+                              progress: 0.6,
+                              startColor: Colors.purple,
+                              endColor: Colors.purple,
+                              width: 4.0,
+                            )),
+                          ),
+                        ),
+                        Center(
+                          child: Text(
+                            widget.currentGame.players[index].points.toString(),
+                            style: TextStyle(
+                              color: Colors.grey.shade700,
+                              fontSize: 32,
+                              shadows: [
+                                Shadow(
+                                  blurRadius: 5.0,
+                                  color: Colors.grey,
+                                  offset: Offset(0.0, 2.5),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -70,5 +103,13 @@ class _PlayerGridState extends State<PlayerGrid> {
         );
       },
     );
+  }
+
+  String randomString(int length) {
+    var rand = new Random();
+    var codeUnits = new List.generate(length, (index) {
+      return rand.nextInt(33) + 89;
+    });
+    return new String.fromCharCodes(codeUnits);
   }
 }
