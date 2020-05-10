a7e31defd39c41d61a3df4aede4c276760151dad
MarinJ <mjuric00@fesb.hr>
2020-05-10 23:50:38 +0200

    FIxes the wrong theme and grey color issue in history_search_page

--- lib/presentation/pages/history_page.dart
+++ lib/presentation/pages/history_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:game_counter/presentation/widgets/info_container.dart';
+
+import '../widgets/info_container.dart';
+import 'history_search_page.dart';
 
 class HistoryPage extends StatefulWidget {
   HistoryPage({Key key}) : super(key: key);
@@ -40,70 +42,6 @@ class _HistoryPageState extends State<HistoryPage> {
             description:
                 'No recent searches available, please use the search bar at the top!'),
       ),
-    );
-  }
-}
-
-class HistorySearchDelegate extends SearchDelegate {
-  @override
-  ThemeData appBarTheme(BuildContext context) {
-    assert(context != null);
-    final ThemeData theme = Theme.of(context);
-    assert(theme != null);
-    return theme;
-  }
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      )
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    if (query.length < 3) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Text('Search term must be longer than two characters'),
-          )
-        ],
-      );
-    }
-
-    //TODO Publish to bloc the query and search the local peristence for that query
-    return Center(
-      child: Text(
-        query,
-        style: TextStyle(color: Colors.red),
-      ),
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    // This method is called everytime the search term changes.
-    // If you want to add search suggestions as the user enters their search term, this is the place to do that.
-    return Text(
-      'test?',
-      style: TextStyle(color: Colors.red),
     );
   }
 }
---
+++ lib/presentation/pages/history_search_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class HistorySearchDelegate extends SearchDelegate<String> {
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    assert(context != null);
+    final ThemeData theme = Theme.of(context);
+    assert(theme != null);
+    return theme.copyWith(
+        inputDecorationTheme: InputDecorationTheme(
+            hintStyle: TextStyle(color: theme.primaryTextTheme.headline6.color)),
+        primaryColor: theme.primaryColor,
+        primaryIconTheme: theme.primaryIconTheme,
+        primaryColorBrightness: theme.primaryColorBrightness,
+        primaryTextTheme: theme.primaryTextTheme,
+        textTheme: theme.textTheme.copyWith(
+            headline6: theme.textTheme.headline6
+                .copyWith(color: theme.primaryTextTheme.headline6.color)));
+  }
+
+  @override
+  String get searchFieldLabel => 'Search Previous Games';
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    if (query.length < 3) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text('Search term must be longer than two characters'),
+          )
+        ],
+      );
+    }
+
+    //TODO Publish to bloc the query and search the local peristence for that query
+    return Center(
+      child: Text(
+        query,
+        style: TextStyle(color: Colors.red),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // This method is called everytime the search term changes.
+    // If you want to add search suggestions as the user enters their search term, this is the place to do that.
+    return Text(
+      'test?',
+      style: TextStyle(color: Colors.red),
+    );
+  }
+}
